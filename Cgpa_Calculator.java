868133dc6175ae4d0e88862fb0bc587a050cef95
Abdulsamad <lasisiabdulsamad9@gmail.com>
2024-02-20 21:53:07 +0100

    printing grades for marks scored

--- Cgpa_Calculator.java
+++ Cgpa_Calculator.java
@@ -7,6 +7,10 @@ public class Cgpa_Calculator {
         int semester = scanner.nextInt();
 
 
+        int totalUnit = 0;
+        int totalQp = 0;
+
+
         for (int i = 0; i < semester; i++) {
             System.out.println("\nEntering details for semester " + (i + 1));
             System.out.print("Enter number of subjects: ");
@@ -19,6 +23,35 @@ public class Cgpa_Calculator {
                 int mark = scanner.nextInt();
                 System.out.print("Enter Credit unit for subject: " + (j + 1) + ": ");
                 int unit = scanner.nextInt();
+
+                String grade;
+
+                if (mark < 100 && mark >= 70) {
+                    grade = "A";
+                }
+                else if (mark <= 69 && mark >= 60) {
+                    grade = "B";
+                }
+                else if (mark <= 59 && mark >= 50) {
+                    grade = "C";
+                }
+                else if (mark <= 49 && mark >= 45) {
+                    grade = "D";
+                }
+                else if (mark <= 44 && mark >= 40) {
+                    grade = "E";
+                }
+                else if (mark <= 39 && mark >= 0) {
+                    grade = "F";
+                }
+
+                else{
+                    System.out.println("\nPlease input correct score " + mark + " is not allowed");
+                    return;
+                }
+
+                System.out.println("Your grade for Subject " + (j + 1) + " is: " + grade);
+
 
             }
         }
